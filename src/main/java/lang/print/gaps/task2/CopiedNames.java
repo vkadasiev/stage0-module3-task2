5714d3ace75a1b53b1070cfeadb02f2dad26a834
Stas Sukora <st.sukora@gmail.com>
2022-11-22 00:32:03 +0300

    Update readme.md

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int firstVariable = 0;
+        int secondVariable = 2000;
+        int thirdVariable = 4000;
+        int fourthVariable = 5000;
+        int fifthVariable = 7000;
+        int firstValue = 1000;
+        int secondValue = 3000;
+        int thirdValue = 90000;
     }
 }
--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
+        int variable = 10;
+        int variable_one = 1;
+        int variable_two = 2;
         int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEfIVE = 5;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+        final int variable_four = 4;
+        final int variable_five = 5;
+        int variable_six = 6;
+        int variable_seven = 7;
+        int variable_eight = 8;
     }
 }
